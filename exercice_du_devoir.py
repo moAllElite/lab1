17e872024b06632d6e344ef64afc327245ae9c61
moallelite <mouhamedn347@gmail.com>
2024-04-25 23:12:54 +0000

    last-commit

--- exercice_du_devoir.py
+++ exercice_du_devoir.py
@@ -6,36 +6,30 @@ from math import sqrt
 
 def my_app(func, arg1, arg2):
     res = []
-    value= 0
+    my_list = []
+    value = 0
     for i, j in zip(arg1, arg2):
-        for x ,y in zip(i, j):
-            value = pow(y, 2) - pow(x, 2)
-   # print(sqrt(value))
-    nombre= sqrt(value)
-    for x, y in zip(arg1, arg2):
-        res.append(nombre)
+        for x, y in zip(i, j):
+            res.append(func(x, y))
+    #print(len(res))
+    for position in range(len(res)):
+        #print(max(res))
+        if res[position] == max(res):
+            my_list.append(res[position])
+
+    #   res.pop(index)
+
     return (("Le nom de la fonction est: {}"
              "\n\t\tles arguments suivante :{} et {}."
              "\n\t\t{} ")
-            .format(func.__name__,  arg1, arg2,res,))
+            .format(func.__name__, arg1, arg2, my_list))
 
 
 def calc_distance(a, b):
-    value = pow(a, 2) - pow(a, 2)
-    #value = 0
-    #for x in a:
-    #    for y in b:
-    #        for item_a in x:
-    #            for item_b in y:
-                    #print(item_a, item_b)
-     #               value = pow(item_b, 2) - pow(item_a, 2)
-    return sqrt(value)
-
-
+    return sqrt(pow(b, 2) - pow(a, 2))
 
 
 print("\n\t\t************************\n")
-
 
 result = my_app(calc_distance, points1, points2)
 print("\t\t" + result)
